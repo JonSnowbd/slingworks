adf190454c4edfd542002341c9cf019b330ac744
Josiah P <vasti009@gmail.com>
2021-09-24 19:39:47 -0300

    Fleshing out debug visualizers a bit.

--- src/debug.zig
+++ src/debug.zig
@@ -12,13 +12,36 @@ pub const DebugType = enum {
     background,
 
     pub fn toColor(self: DebugType) sling.math.Vec4 {
-        switch(self) {
-            .success => {return sling.Theme.primary;}
+        switch (self) {
+            .success => {
+                return sling.Theme.debugSuccess;
+            },
+            .err => {
+                return sling.Theme.debugError;
+            },
+            .warning => {
+                return sling.Theme.debugWarning;
+            },
+            .primary => {
+                return sling.Theme.primary;
+            },
+            .background => {
+                return sling.Theme.highlight;
+            },
+            // else => {
+            //     return sling.Theme.secondary;
+            // }
         }
     }
 };
 
+//todo: more debug convenience methods
+
 /// Renders an in world dot unaffected by zoom.
 pub fn dot(center: sling.math.Vec2, radius: f32, debugMethod: DebugType) void {
-    sling.render.circle(.world, sling.render.debugDepth, center, radius/sling.render.camera.zoom, sling.Theme.debugInfo);
+    sling.render.circle(.world, sling.render.debugDepth, center, radius / sling.render.camera.zoom, debugMethod.toColor());
+}
+
+pub fn rect(rectangle: sling.math.Rect, debugMethod: DebugType) void {
+    sling.render.rectangle(.world, sling.render.debugDepth, rectangle, debugMethod.toColor(), null);
 }
